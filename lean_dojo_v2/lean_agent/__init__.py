36b6db073b9fcc80a59fde68a97af92a7b97b353
mcrco <marco.yang.ca@gmail.com>
2026-02-23 19:53:31 -0800

    import fix

--- lean_dojo_v2/lean_agent/__init__.py
+++ lean_dojo_v2/lean_agent/__init__.py
@@ -2,7 +2,7 @@ __version__ = "1.0.0"
 __author__ = "LeanDojo-v2 Contributors"
 
 from .config import ProverConfig, TrainingConfig
-from .database.dynamic_database import DynamicDatabase
+from ..database import DynamicDatabase
 
 __all__ = [
     "DynamicDatabase",
